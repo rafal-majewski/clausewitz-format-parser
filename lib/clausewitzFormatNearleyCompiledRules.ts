902533908ae3143d2037c99da1c8a24cbfe28c05
Rafał Majewski <goodheropl@gmail.com>
2023-07-25 16:16:58 +0200

    Parsing non empty text

--- lib/clausewitzFormatNearleyCompiledRules.ts
+++ lib/clausewitzFormatNearleyCompiledRules.ts
@@ -4,7 +4,20 @@ export const clausewitzFormatNearleyCompiledRules: nearley.CompiledRules = {
 	Lexer: undefined,
 	ParserRules: [
 		{"name": "CLAUSEWITZ_FORMAT", "symbols": []},
-		{"name": "CLAUSEWITZ_FORMAT", "symbols": [/./]},
+		{"name": "CLAUSEWITZ_FORMAT", "symbols": ["NON_EMPTY_STRING"]},
+		{"name": "NON_EMPTY_STRING$ebnf$1", "symbols": [/./]},
+		{
+			"name": "NON_EMPTY_STRING$ebnf$1",
+			"symbols": ["NON_EMPTY_STRING$ebnf$1", /./],
+			"postprocess": function arrpush(d) {
+				return d[0].concat([d[1]]);
+			},
+		},
+		{
+			"name": "NON_EMPTY_STRING",
+			"symbols": ["NON_EMPTY_STRING$ebnf$1"],
+			"postprocess": (d) => d[0].join(""),
+		},
 	],
 	ParserStart: "CLAUSEWITZ_FORMAT",
 };
--- test/NearleyClausewitzFormatParser.test.ts
+++ test/NearleyClausewitzFormatParser.test.ts
@@ -5,6 +5,7 @@ describe("NearleyClausewitzFormatParser", () => {
 	test.each([
 		["", []],
 		["a", ["a"]],
+		["abc", ["abc"]],
 	])("parse(%p)", (text, expected) => {
 		const parser = new NearleyClausewitzFormatParser();
 		expect(parser.parse(text)).toEqual(expected);
