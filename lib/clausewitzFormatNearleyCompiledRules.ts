27eef8eb52dff121043fc5744e0d7e91a8491c31
Rafał Majewski <goodheropl@gmail.com>
2023-07-25 16:14:33 +0200

    Parsing one letter text

--- lib/clausewitzFormatNearleyCompiledRules.ts
+++ lib/clausewitzFormatNearleyCompiledRules.ts
@@ -2,6 +2,9 @@ import nearley from "nearley";
 
 export const clausewitzFormatNearleyCompiledRules: nearley.CompiledRules = {
 	Lexer: undefined,
-	ParserRules: [{"name": "CLAUSEWITZ_FORMAT", "symbols": []}],
+	ParserRules: [
+		{"name": "CLAUSEWITZ_FORMAT", "symbols": []},
+		{"name": "CLAUSEWITZ_FORMAT", "symbols": [/./]},
+	],
 	ParserStart: "CLAUSEWITZ_FORMAT",
 };
--- test/NearleyClausewitzFormatParser.test.ts
+++ test/NearleyClausewitzFormatParser.test.ts
@@ -2,7 +2,10 @@ import {describe, test, expect} from "vitest";
 import {NearleyClausewitzFormatParser} from "../lib/NearleyClausewitzFormatParser.js";
 
 describe("NearleyClausewitzFormatParser", () => {
-	test.each([["", []]])("parse(%p)", (text, expected) => {
+	test.each([
+		["", []],
+		["a", ["a"]],
+	])("parse(%p)", (text, expected) => {
 		const parser = new NearleyClausewitzFormatParser();
 		expect(parser.parse(text)).toEqual(expected);
 	});
