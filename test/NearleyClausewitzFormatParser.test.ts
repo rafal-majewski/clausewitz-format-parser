abf07662d3f7896d26306e41e18cb32599ce9928
Rafał Majewski <goodheropl@gmail.com>
2023-08-05 12:42:00 +0200

    Parsing strings without a whitespace before

--- test/NearleyClausewitzFormatParser.test.ts
+++ test/NearleyClausewitzFormatParser.test.ts
@@ -29,6 +29,7 @@ describe("NearleyClausewitzFormatParser", async () => {
 		[`a = "b"`, ["a", "=", `"b"`]],
 		[`a = "b c"`, ["a", "=", `"b c"`]],
 		[`a = "b\\"c"`, ["a", "=", `"b\\"c"`]],
+		[`a="b"`, [`a="b"`]],
 	])("parse(%p)", (text, expected) => {
 		const parser = new NearleyClausewitzFormatParser();
 		expect(parser.parse(text)).toEqual(expected);
