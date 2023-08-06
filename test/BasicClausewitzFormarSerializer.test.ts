160e547db443fd82163c5b0c55cc7e6d48ea15e0
Rafał Majewski <goodheropl@gmail.com>
2023-08-06 19:43:35 +0200

    Testing serializer on HoI4 focus trees

--- test/BasicClausewitzFormarSerializer.test.ts
+++ test/BasicClausewitzFormarSerializer.test.ts
@@ -1,5 +1,9 @@
 import {describe, test, expect} from "vitest";
 import {BasicClausewitzFormarSerializer} from "../lib/BasicClausewitzFormarSerializer.js";
+import * as fs from "fs/promises";
+import * as path from "path";
+import * as url from "url";
+import {NearleyClausewitzFormatParser} from "../lib/NearleyClausewitzFormatParser.js";
 
 describe("BasicClausewitzFormarSerializer", async () => {
 	test.each([
@@ -19,4 +23,47 @@ describe("BasicClausewitzFormarSerializer", async () => {
 		const actual = serializer.serialize(clausewitzFormatObject);
 		expect(actual).toBe(expected);
 	});
+
+	const heartsOfIronIVFocusTreeFileContents = await Promise.all(
+		await fs
+			.readdir(
+				path.join(
+					path.dirname(url.fileURLToPath(import.meta.url)),
+					"data",
+					"hearts_of_iron_iv_focus_trees",
+				),
+			)
+			.then((fileNames) =>
+				fileNames.map((fileName) =>
+					fs
+						.readFile(
+							path.join(
+								path.dirname(url.fileURLToPath(import.meta.url)),
+								"data",
+								"hearts_of_iron_iv_focus_trees",
+								fileName,
+							),
+							"utf8",
+						)
+						.then((fileContents) => [fileName, fileContents] as const),
+				),
+			),
+	);
+	test.each(heartsOfIronIVFocusTreeFileContents)(
+		`parse HoI4 focus tree file "%p"`,
+		async (fileName, fileContents) => {
+			const parser = new NearleyClausewitzFormatParser();
+			const parsedFileContents = parser.parse(fileContents);
+			const serializer = new BasicClausewitzFormarSerializer({
+				betweenItems: (indentationLevel) => "\n" + "\t".repeat(indentationLevel),
+				afterOpenBrace: (indentationLevel) => "\n" + "\t".repeat(indentationLevel),
+				beforeCloseBrace: (indentationLevel) => "\n" + "\t".repeat(indentationLevel - 1),
+				betweenBracesInEmptyObject: () => "",
+			});
+			const serializedParsedFileContents = serializer.serialize(parsedFileContents);
+			const parsedSerializedParsedFileContents = parser.parse(serializedParsedFileContents);
+
+			expect(parsedSerializedParsedFileContents).toStrictEqual(parsedFileContents);
+		},
+	);
 });
