3a0785d3564f48ca56e78f01e03bb77978fdcc95
Rafał Majewski <goodheropl@gmail.com>
2023-08-06 18:19:13 +0200

    Serializing object with just a string

--- test/BasicClausewitzFormarSerializer.test.ts
+++ test/BasicClausewitzFormarSerializer.test.ts
@@ -2,7 +2,10 @@ import {describe, test, expect} from "vitest";
 import {BasicClausewitzFormarSerializer} from "../lib/BasicClausewitzFormarSerializer.js";
 
 describe("BasicClausewitzFormarSerializer", async () => {
-	test.each([[[], "\n"]])("serialize(%p)", (clausewitzFormatObject, expected) => {
+	test.each([
+		[[], "\n"],
+		[["a"], "a\n"],
+	])("serialize(%p)", (clausewitzFormatObject, expected) => {
 		const serializer = new BasicClausewitzFormarSerializer();
 		const actual = serializer.serialize(clausewitzFormatObject);
 		expect(actual).toBe(expected);
